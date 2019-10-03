f0e2f85ad699827d6ad623d7fbffbe079742163a
Espen Klem <espen.klem@gmail.com>
2019-10-03 21:28:21 +0200

    A bit further on truncating

--- test/map.js
+++ test/map.js
@@ -1,43 +1,56 @@
-const item = ['love', 'and', 'theft', 'er', 'det', '31', 'studioalbumet', 'til', 'bob', 'dylan', 'og', 'ble', 'gitt', 'ut', 'gjennom', 'columbia', 'records', 'i', 'september', '2001', 'albumet', 'fortsatte', 'det', 'kunstneriske', 'comebacket', 'etter', 'time', 'out', 'of', 'mind', 'i', '1997', 'og', 'fekk', 'enda', 'bedre', 'mottakelse', 'enn', 'det', 'forrige', 'den', 'korrekte', 'tittelen', 'på', 'albumet', 'er', 'love', 'and', 'theft', 'med', 'engelske', 'anførselstegn', 'tittelen', 'var', 'visstnok', 'inspirert', 'av', 'en', 'bok', 'skrevet', 'av', 'historikeren', 'eric', 'lott', 'love', 'theft', 'blackface', 'minstrelsy', 'and', 'the', 'american', 'working', 'class', 'som', 'kom', 'ut', 'i', '1993', 'i', '2003', 'var', 'albumet', 'plassert', 'på', '467', 'plass', 'på', 'listen', 'til', 'rolling', 'stone', 'over', 'de', '500', 'største', 'albumene', 'noensinne', 'mens', 'newsweek', 'kåret', 'det', 'til', 'det', 'nest', 'beste', 'albumet', 'det', 'tiåret', '5', 'albumet', 'gikk', 'til', 'topps', 'i', 'norge', 'noen', 'utgaver', 'av', 'cden', 'ble', 'gitt', 'ut', 'med', 'en', 'bonusplate', 'med', 'to', 'spor', 'som', 'ikke', 'var', 'utgitt', 'før']
-const query = ['bob', 'dylan', 'albumet', 'bonsuplate', 'tiåret']
+const query = ['theft', 'bob', 'dylan', 'albumet', 'bonsuplate', 'tiåret']
+const result = ['love', 'and', 'theft', 'er', 'det', '31', 'studioalbumet', 'til', 'bob', 'dylan', 'og', 'ble', 'gitt', 'ut', 'gjennom', 'columbia', 'records', 'i', 'september', '2001', 'albumet', 'fortsatte', 'det', 'kunstneriske', 'comebacket', 'etter', 'time', 'out', 'of', 'mind', 'i', '1997', 'og', 'fekk', 'enda', 'bedre', 'mottakelse', 'enn', 'det', 'forrige', 'den', 'korrekte', 'tittelen', 'på', 'albumet', 'er', 'love', 'and', 'theft', 'med', 'engelske', 'anførselstegn', 'tittelen', 'var', 'visstnok', 'inspirert', 'av', 'en', 'bok', 'skrevet', 'av', 'historikeren', 'eric', 'lott', 'love', 'theft', 'blackface', 'minstrelsy', 'and', 'the', 'american', 'working', 'class', 'som', 'kom', 'ut', 'i', '1993', 'i', '2003', 'var', 'albumet', 'plassert', 'på', '467', 'plass', 'på', 'listen', 'til', 'rolling', 'stone', 'over', 'de', '500', 'største', 'albumene', 'noensinne', 'mens', 'newsweek', 'kåret', 'det', 'til', 'det', 'nest', 'beste', 'albumet', 'det', 'tiåret', '5', 'albumet', 'gikk', 'til', 'topps', 'i', 'norge', 'noen', 'utgaver', 'av', 'cden', 'ble', 'gitt', 'ut', 'med', 'en', 'bonusplate', 'med', 'to', 'spor', 'som', 'ikke', 'var', 'utgitt', 'før']
+const properties = { itemMaxWords: 50 }
 
 const defaultProperties = {
   itemMaxWords: 0,
+  truncate: false,
+  truncateStart: '...',
+  truncateEnd: '...',
   hitCount: 0,
-  hitPadding: 3,
+  hitPaddingMin: 3,
   highlightStart: '<span class="">',
-  highlightEnd: '</span>'
+  highlightEnd: '</span>',
+  space: ' '
 }
 
-const properties = {
-  itemMaxWords: 0
-}
-
-const highlight = function (query, items, properties) {
+const highlight = function (queryArr, itemArr, properties) {
   properties = {
     ...defaultProperties,
     ...properties
   }
 
-  items.map(function (itemWord) {
-    if (query.find(function (queryWord) { return itemWord === queryWord })) {
-      console.log(itemWord)
+  // Check if item is to be truncated
+  if (itemArr.length > properties.itemMaxWords && properties.itemMaxWords !== 0) {
+    properties.truncate = true
+  }
+
+  // Set matched words to highlightable (true)
+  itemArr = itemArr.map(function (itemWord) {
+    const wordObj = {
+      word: itemWord,
+      highlightable: false
+    }
+    if (queryArr.find(function (queryWord) { return itemWord === queryWord })) {
+      wordObj.highlightable = true
       properties.hitCount += 1
     }
+    console.log(wordObj)
+    return wordObj
   })
 
   console.log('hitCount: ' + properties.hitCount)
 
-  if (items.length >= properties.itemMaxWords && properties.itemMaxWords !== 0) {
-    console.log(items.length + ' >= ' + properties.itemMaxWords)
-    truncate(items)
+  if (itemArr.length >= properties.itemMaxWords && properties.itemMaxWords !== 0) {
+    console.log(itemArr.length + ' >= ' + properties.itemMaxWords)
+    truncate(itemArr)
   } else {
-    console.log(items.length + ' < ' + properties.itemMaxWords + ' OR itemMaxWords === 0')
+    console.log(itemArr.length + ' < ' + properties.itemMaxWords + ' OR itemMaxWords === 0')
   }
 }
 
-const truncate = function (items) {
-  console.log('truncate: ' + items)
+const truncate = function (item) {
+  console.log(JSON.stringify(item))
 }
 
-highlight(query, item, properties)
+highlight(query, result, properties)
