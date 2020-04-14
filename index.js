5285c3c5bb6a03a7d04a4b84e7a3821e62c311d9
Espen Klem <espen.klem@gmail.com>
2020-04-14 22:17:12 +0200

    New working version * Old tests fixed * Some new added
    Still missing documentation

--- index.js
+++ index.js
@@ -1,46 +1,211 @@
-// Default hightlight code object
-const defaultHighlighting = {
-  start: '<span class="highlighted">',
-  end: '</span>'
+const defaultProperties = {
+  itemMaxWords: 0,
+  truncateStart: '',
+  truncateEnd: '... ',
+  hitPaddingMin: 5,
+  highlightStart: '<span class="hitHighlight">',
+  highlightEnd: '</span>',
+  divider: ' '
 }
 
-const highlight = function (query, item, highlighting) {
-  const itemHighlighted = [...item]
-  // Check input
-  // Populate regex and options objects
-  highlighting = {
-    ...defaultHighlighting,
-    ...highlighting
+const highlight = function (queryArr, itemArr, properties) {
+  properties = {
+    ...defaultProperties,
+    ...properties
   }
-  // Compare all query array values with all item array values
-  for (let i = 0; i < query.length; i++) {
-    for (let j = 0; j < itemHighlighted.length; j++) {
-      if (query[i] === itemHighlighted[j]) {
-        itemHighlighted[j] = highlighting.start + itemHighlighted[j] + highlighting.end
-      }
+
+  const cases = {
+    hitCount: 0,
+    truncate: false,
+    keepAllQueryWords: true,
+    toLittlePadding: false
+  }
+
+  let hitArr = []
+  let hitTruncatedArr = []
+  let itemHighlighted = ''
+
+  // ### Preparing & preprocessing for hit highlighting ###
+  // A: Check if item is to be truncated
+  cases.truncate = caseTruncate(itemArr.length, properties)
+
+  // B: Set matched words to highlightable (true) in itemArr
+  itemArr = setHighlightables(itemArr, queryArr, cases)
+
+  // C: Joining neighbour highlightable words, removing redundant and setting index value
+  //    And add highlight start and end
+  itemArr = joinNeighbourHighlightable(itemArr, properties)
+  itemArr = addHighlighting(itemArr, properties)
+
+  // D: Defining index on highlightable words and pushing to hitArr
+  hitArr = itemArr.filter(itemArr => itemArr.highlightable)
+
+  // E: Check if to keep all query wordsCut off hit array
+  cases.keepAllQueryWords = caseKeepAllQueryWords(hitArr, properties)
+
+  // ### The highlighter "switch" ###
+  if (!cases.truncate) {
+    // just return the highlighted itemArray
+    console.log('### Case - Just highlight ###')
+    itemHighlighted = getHighlightedString(itemArr, properties.divider)
+    return itemHighlighted
+  } else if (cases.truncate && cases.keepAllQueryWords) {
+    // needs truncating, but keep all query words
+    console.log('### Case - Truncat but keep all query word hits ###')
+    properties.hitPaddingMin = properties.hitPaddingMin + expandPaddingMin(hitArr.length, itemArr.length, properties)
+    hitArr = setPaddingStartEnd(hitArr, itemArr.length, properties)
+    hitArr = joinOverlappingTruncate(hitArr)
+    hitTruncatedArr = truncateHitArr(hitArr, itemArr, hitTruncatedArr)
+    itemHighlighted = getTruncatedHighlightedString(hitTruncatedArr, properties.truncateStart, properties.truncateEnd, properties.divider)
+    return itemHighlighted
+  } else if (cases.truncate && !cases.keepAllQueryWords) {
+    // needs truncadting and have to cut off query wors
+    console.log('### Case - Truncat and cut off some query word hits ###')
+    hitArr = cutOffHitArray(hitArr, properties)
+    hitArr = setPaddingStartEnd(hitArr, itemArr.length, properties)
+    hitArr = joinOverlappingTruncate(hitArr)
+    hitTruncatedArr = truncateHitArr(hitArr, itemArr, hitTruncatedArr)
+    itemHighlighted = getTruncatedHighlightedString(hitTruncatedArr, properties.truncateStart, properties.truncateEnd, properties.divider)
+    return itemHighlighted
+  }
+}
+
+// ###################################
+// # The Functions to make it happen #
+// ###################################
+
+// Function: figure out if itemArr needs to be truncated
+const caseTruncate = function (itemArrLength, properties, cases) {
+  if (itemArrLength > properties.itemMaxWords && properties.itemMaxWords !== 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
+// Function: Set highlightable query words and count how many
+const setHighlightables = function (itemArr, queryArr, cases) {
+  itemArr = itemArr.map(function (itemWord, index) {
+    const wordObj = {
+      word: itemWord
+    }
+    if (queryArr.find(function (queryWord) { return itemWord === queryWord })) {
+      wordObj.highlightable = true
+      cases.hitCount = cases.hitCount + 1
+    }
+    return wordObj
+  })
+  return itemArr
+}
+
+// Function: Joining neighbour highlightable words, removing redundant and setting index value
+const joinNeighbourHighlightable = function (itemArr, properties) {
+  for (let i = 0; i < itemArr.length; i++) {
+    if (i > 0 && itemArr[i].highlightable && itemArr[i - 1].highlightable) {
+      // Joining this and previous word
+      itemArr[i].word = itemArr[i - 1].word + properties.divider + itemArr[i].word
+      // Removing previous word from array
+      itemArr.splice(i - 1, 1)
+      // fixing array count
+      i = i - 1
+    }
+    // setting index on all highlightable
+    if (itemArr[i].highlightable) {
+      itemArr[i].index = i
     }
   }
-  return itemHighlighted
+  return itemArr
 }
 
-// const defaultProperties = {
-//  highlightStart: '<span class="">',
-//  highlightEnd: '</span>',
-//  itemMaxWords: '3',
-//  minHightlightPadding: '5'
-// }
+// Function: Add highlighting start and end to highlightable
+const addHighlighting = function (itemArr, properties) {
+  itemArr = itemArr.map(function (itemWord, index) {
+    if (itemWord.highlightable) {
+      itemWord.word = properties.highlightStart + itemWord.word + properties.highlightEnd
+    }
+    return itemWord
+  })
+  return itemArr
+}
 
-// const newHighlight = function (query, item, properties) {
-//   properties = {
-//     ...defaultProperties,
-//     ...properties
-//   }
-// }
+// Just set index for padding start and end for each hit
+const setPaddingStartEnd = function (hitArr, itemArrLength, properties) {
+  for (let i = 0; i < hitArr.length; i++) {
+    // do some padding stuff...
+    hitArr[i].paddStart = Math.max(hitArr[i].index - properties.hitPaddingMin, 0)
+    hitArr[i].paddEnd = Math.min(hitArr[i].index + properties.hitPaddingMin + 1, itemArrLength)
+  }
+  return hitArr
+}
 
-// Checks on item
-//  if longer than maxWords
-//    --> look up all hits in item
-//    --> join hightlightAble words
+// Check if not to keep all query word hits
+const caseKeepAllQueryWords = function (hitArr, properties) {
+  if (hitArr.length * ((properties.hitPaddingMin * 2) + 1) > properties.itemMaxWords) {
+    return false
+  } else {
+    return true
+  }
+}
+
+// Cut off hit array
+const cutOffHitArray = function (hitArr, properties) {
+  // per hit
+  const totalWords = hitArr.length * ((properties.hitPaddingMin * 2) + 1)
+  const wordsPerHit = (properties.hitPaddingMin * 2) + 1
+  const wordsToMany = totalWords - properties.itemMaxWords
+  //  how many hits to cut off
+  const hitsToKeep = hitArr.length - (Math.ceil(wordsToMany / wordsPerHit))
+  hitArr = hitArr.slice(0, hitsToKeep)
+  return hitArr
+}
+
+const expandPaddingMin = function (hitLength, itemLength, properties) {
+  return Math.floor((properties.itemMaxWords - ((hitLength * (properties.hitPaddingMin * 2)) + 1)) / hitLength / 2)
+}
+
+const joinOverlappingTruncate = function (hitArr) {
+  for (let i = 0; i < hitArr.length; i++) {
+    if (i > 0 && hitArr[i].paddStart <= hitArr[i - 1].paddEnd) {
+      // join this and next
+      hitArr[i].paddStart = hitArr[i - 1].paddStart
+      // Removing previous padding group from index
+      hitArr.splice(i - 1, 1)
+      // fixing array count
+      i = i - 1
+    }
+  }
+  return hitArr
+}
+
+// Generate Truncate groups
+const truncateHitArr = function (hitArr, itemArr, hitTruncatedArr) {
+  for (let i = 0; i < hitArr.length; i++) {
+    hitTruncatedArr[i] = itemArr.slice(hitArr[i].paddStart, hitArr[i].paddEnd)
+  }
+  return hitTruncatedArr
+}
+
+// Join words + divider for highlighted full itemArr
+const getHighlightedString = function (itemArr, divider) {
+  let hitFull = ''
+  for (let i = 0; i < itemArr.length; i++) {
+    hitFull += itemArr[i].word + divider
+  }
+  return hitFull
+}
+
+// Create the string with hit highligted, truncated and padded
+// Guessing this may need to have it's siblings for not truncated ???
+const getTruncatedHighlightedString = function (hitTruncatedArr, truncateStart, truncateEnd, divider) {
+  let hitTruncated = []
+  for (let i = 0; i < hitTruncatedArr.length; i++) {
+    let singleHitPadded = ''
+    for (let j = 0; j < hitTruncatedArr[i].length; j++) {
+      singleHitPadded += hitTruncatedArr[i][j].word + divider
+    }
+    hitTruncated += truncateStart + singleHitPadded + truncateEnd
+  }
+  return hitTruncated
+}
 
 module.exports = highlight
-// module.exports = newHighlight
--- test/test.js
+++ test/test.js
@@ -4,16 +4,22 @@ const test = require('tape')
 
 // data
 const item = ['some', 'interesting', 'words', 'to', 'remember']
+const itemLong = ['love', 'and', 'theft', 'er', 'det', '31', 'studioalbumet', 'til', 'bob', 'dylan', 'og', 'ble', 'gitt',
+  'ut', 'gjennom', 'columbia', 'records', 'i', 'september', '2001', 'albumet', 'fortsatte', 'det', 'kunstneriske',
+  'comebacket', 'etter', 'time', 'out', 'of', 'mind', 'i', '1997', 'og', 'fekk', 'enda', 'bedre', 'mottakelse', 'enn',
+  'det', 'forrige', 'den', 'korrekte', 'tittelen', 'på', 'albumet', 'er', 'love', 'and', 'theft', 'med', 'engelske',
+  'anførselstegn', 'tittelen', 'var', 'visstnok', 'inspirert', 'av', 'en', 'bok', 'skrevet', 'av', 'historikeren',
+  'eric', 'lott', 'love', 'theft', 'blackface', 'minstrelsy', 'and', 'the', 'american', 'working', 'class', 'som',
+  'kom', 'ut', 'i', '1993', 'i', '2003', 'var', 'albumet', 'plassert', 'på', '467', 'plass', 'på', 'listen', 'til',
+  'rolling', 'stone', 'over', 'de', '500', 'største', 'albumene', 'noensinne', 'mens', 'newsweek', 'kåret', 'det',
+  'til', 'det', 'nest', 'beste', 'albumet', 'det', 'tiåret', '5', 'albumet', 'gikk', 'til', 'topps', 'i', 'norge',
+  'noen', 'utgaver', 'av', 'cden', 'ble', 'gitt', 'ut', 'med', 'en', 'bonusplate', 'med', 'to', 'spor', 'som', 'ikke',
+  'var', 'utgitt', 'før']
 
 test('default highlighting', function (t) {
   t.plan(1)
   const query = ['interesting', 'words']
-  const expectedResult = ['some',
-    '<span class="highlighted">interesting</span>',
-    '<span class="highlighted">words</span>',
-    'to',
-    'remember'
-  ]
+  const expectedResult = 'some <span class="hitHighlight">interesting words</span> to remember '
   const actualResult = highlight(query, item)
   t.looseEqual(actualResult, expectedResult)
 })
@@ -21,12 +27,7 @@ test('default highlighting', function (t) {
 test('no hit so no highlighting', function (t) {
   t.plan(1)
   const query = ['no', 'hits']
-  const expectedResult = ['some',
-    'interesting',
-    'words',
-    'to',
-    'remember'
-  ]
+  const expectedResult = 'some interesting words to remember '
   const actualResult = highlight(query, item)
   t.looseEqual(actualResult, expectedResult)
 })
@@ -34,13 +35,31 @@ test('no hit so no highlighting', function (t) {
 test('markdown highlighting', function (t) {
   t.plan(1)
   const query = ['interesting', 'words']
-  const expectedResult = [
-    'some',
-    '**interesting**',
-    '**words**',
-    'to',
-    'remember'
-  ]
-  const actualResult = highlight(query, item, { start: '**', end: '**' })
+  const expectedResult = 'some **interesting words** to remember '
+  const actualResult = highlight(query, item, { highlightStart: '**', highlightEnd: '**' })
+  t.looseEqual(actualResult, expectedResult)
+})
+
+test('Long item, no truncating', function (t) {
+  t.plan(1)
+  const query = ['studioalbumet', 'mottakelse', 'bedre', 'dylan', 'bob', 'working', 'class']
+  const expectedResult = 'love and theft er det 31 <span class="hitHighlight">studioalbumet</span> til <span class="hitHighlight">bob dylan</span> og ble gitt ut gjennom columbia records i september 2001 albumet fortsatte det kunstneriske comebacket etter time out of mind i 1997 og fekk enda <span class="hitHighlight">bedre mottakelse</span> enn det forrige den korrekte tittelen på albumet er love and theft med engelske anførselstegn tittelen var visstnok inspirert av en bok skrevet av historikeren eric lott love theft blackface minstrelsy and the american <span class="hitHighlight">working class</span> som kom ut i 1993 i 2003 var albumet plassert på 467 plass på listen til rolling stone over de 500 største albumene noensinne mens newsweek kåret det til det nest beste albumet det tiåret 5 albumet gikk til topps i norge noen utgaver av cden ble gitt ut med en bonusplate med to spor som ikke var utgitt før '
+  const actualResult = highlight(query, itemLong)
+  t.looseEqual(actualResult, expectedResult)
+})
+
+test('Long item, truncating, but no cutoff', function (t) {
+  t.plan(1)
+  const query = ['studioalbumet', 'mottakelse', 'bedre', 'dylan', 'bob', 'working', 'class']
+  const expectedResult = 'love and theft er det 31 <span class="hitHighlight">studioalbumet</span> til <span class="hitHighlight">bob dylan</span> og ble gitt ut gjennom columbia records i september ... time out of mind i 1997 og fekk enda <span class="hitHighlight">bedre mottakelse</span> enn det forrige den korrekte tittelen på albumet er ... eric lott love theft blackface minstrelsy and the american <span class="hitHighlight">working class</span> som kom ut i 1993 i 2003 var albumet ... '
+  const actualResult = highlight(query, itemLong, { itemMaxWords: 80 })
+  t.looseEqual(actualResult, expectedResult)
+})
+
+test('Long item, truncating, and cutoff query matches', function (t) {
+  t.plan(1)
+  const query = ['studioalbumet', 'mottakelse', 'bedre', 'dylan', 'bob', 'working', 'class']
+  const expectedResult = 'and theft er det 31 <span class="hitHighlight">studioalbumet</span> til <span class="hitHighlight">bob dylan</span> og ble gitt ut gjennom ... i 1997 og fekk enda <span class="hitHighlight">bedre mottakelse</span> enn det forrige den korrekte ... '
+  const actualResult = highlight(query, itemLong, { itemMaxWords: 40 })
   t.looseEqual(actualResult, expectedResult)
 })
