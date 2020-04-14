fb0b8b7bcf00f2fdee9f8b1331f36c8c4a7f1776
Espen Klem <espen.klem@gmail.com>
2020-04-14 17:41:26 +0200

    Mostly working truncating and padding Still need to: * Expand paddingMin when room for it * Stringifyer for when only highlighting, but keeping the full result * Check if inputs are arrays * Export function * Create a test

--- test/map.js
+++ test/map.js
@@ -1,51 +1,152 @@
-const query = ['studioalbumet', 'mottakelse', 'bedre']
-const result = ['love', 'and', 'theft', 'er', 'det', '31', 'studioalbumet', 'til', 'bob', 'dylan', 'og', 'ble', 'gitt', 'ut', 'gjennom', 'columbia', 'records', 'i', 'september', '2001', 'albumet', 'fortsatte', 'det', 'kunstneriske', 'comebacket', 'etter', 'time', 'out', 'of', 'mind', 'i', '1997', 'og', 'fekk', 'enda', 'bedre', 'mottakelse', 'enn', 'det', 'forrige', 'den', 'korrekte', 'tittelen', 'på', 'albumet', 'er', 'love', 'and', 'theft', 'med', 'engelske', 'anførselstegn', 'tittelen', 'var', 'visstnok', 'inspirert', 'av', 'en', 'bok', 'skrevet', 'av', 'historikeren', 'eric', 'lott', 'love', 'theft', 'blackface', 'minstrelsy', 'and', 'the', 'american', 'working', 'class', 'som', 'kom', 'ut', 'i', '1993', 'i', '2003', 'var', 'albumet', 'plassert', 'på', '467', 'plass', 'på', 'listen', 'til', 'rolling', 'stone', 'over', 'de', '500', 'største', 'albumene', 'noensinne', 'mens', 'newsweek', 'kåret', 'det', 'til', 'det', 'nest', 'beste', 'albumet', 'det', 'tiåret', '5', 'albumet', 'gikk', 'til', 'topps', 'i', 'norge', 'noen', 'utgaver', 'av', 'cden', 'ble', 'gitt', 'ut', 'med', 'en', 'bonusplate', 'med', 'to', 'spor', 'som', 'ikke', 'var', 'utgitt', 'før']
-const properties = { itemMaxWords: 100 }
+const query = ['studioalbumet', 'mottakelse', 'bedre', 'dylan', 'bob', 'working', 'class']
+const result = ['love', 'and', 'theft', 'er', 'det', '31', 'studioalbumet', 'til', 'bob', 'dylan', 'og', 'ble', 'gitt',
+  'ut', 'gjennom', 'columbia', 'records', 'i', 'september', '2001', 'albumet', 'fortsatte', 'det', 'kunstneriske',
+  'comebacket', 'etter', 'time', 'out', 'of', 'mind', 'i', '1997', 'og', 'fekk', 'enda', 'bedre', 'mottakelse', 'enn',
+  'det', 'forrige', 'den', 'korrekte', 'tittelen', 'på', 'albumet', 'er', 'love', 'and', 'theft', 'med', 'engelske',
+  'anførselstegn', 'tittelen', 'var', 'visstnok', 'inspirert', 'av', 'en', 'bok', 'skrevet', 'av', 'historikeren',
+  'eric', 'lott', 'love', 'theft', 'blackface', 'minstrelsy', 'and', 'the', 'american', 'working', 'class', 'som',
+  'kom', 'ut', 'i', '1993', 'i', '2003', 'var', 'albumet', 'plassert', 'på', '467', 'plass', 'på', 'listen', 'til',
+  'rolling', 'stone', 'over', 'de', '500', 'største', 'albumene', 'noensinne', 'mens', 'newsweek', 'kåret', 'det',
+  'til', 'det', 'nest', 'beste', 'albumet', 'det', 'tiåret', '5', 'albumet', 'gikk', 'til', 'topps', 'i', 'norge',
+  'noen', 'utgaver', 'av', 'cden', 'ble', 'gitt', 'ut', 'med', 'en', 'bonusplate', 'med', 'to', 'spor', 'som', 'ikke',
+  'var', 'utgitt', 'før']
+const properties = { itemMaxWords: 135 }
 
-const defaultProperties = {
-  itemMaxWords: 0,
-  truncate: false,
-  truncateStart: '',
-  truncateEnd: '... ',
-  hitPaddingMin: 5,
-  highlightStart: '<span class="hitHighlight">',
-  highlightEnd: '</span>',
-  divider: ' '
-}
+console.log(query)
 
 const highlight = function (queryArr, itemArr, properties) {
+  const defaultProperties = {
+    itemMaxWords: 0,
+    truncateStart: '...',
+    truncateEnd: '... ',
+    hitPaddingMin: 5,
+    highlightStart: '<span class="hitHighlight">',
+    highlightEnd: '</span>',
+    divider: ' '
+  }
+
   properties = {
     ...defaultProperties,
     ...properties
   }
-  let hitArr = []
-  const hitPaddedArr = []
-  let hitCount = 0
-  const hitPadding = properties.hitPaddingMin
+  console.log(properties)
 
-  console.log('query array: ' + queryArr)
-  console.log('result array: ' + itemArr)
-
-  // A: Check if item is to be truncated
-  if (itemArr.length > properties.itemMaxWords && properties.itemMaxWords !== 0) {
-    properties.truncate = true
-    // console.log('properties.truncate: ' + properties.truncate)
+  const cases = {
+    hitCount: 0,
+    truncate: false,
+    keepAllQueryWords: true,
+    toLittlePadding: false
   }
 
-  // B: Set matched words to highlightable (true)
+  let hitArr = []
+  let hitTruncatedArr = []
+  let item = ''
+
+  // ##################################################
+  // # Preparing & preprocessing for hit highlighting #
+  // ##################################################
+
+  // A: Check if item is to be truncated
+  if (caseTruncate(itemArr.length, properties)) {
+    cases.truncate = true
+    console.log('Truncate: true')
+  } else {
+    console.log('Truncate: false')
+  }
+
+  // B: Set matched words to highlightable (true) in itemArr
+  itemArr = setHighlightables(itemArr, queryArr, cases)
+  console.log('hitCount: ' + cases.hitCount)
+
+  // C: Joining neighbour highlightable words, removing redundant and setting index value
+  //    And add highlight start and end
+  itemArr = joinNeighbourHighlightable(itemArr, properties)
+  itemArr = addHighlighting(itemArr, properties)
+
+  // D: Defining index on highlightable words and pushing to hitArr
+  hitArr = itemArr.filter(itemArr => itemArr.highlightable)
+
+  // E: Calculating padding array of arrays
+  //    Caluclataing how much padding
+  //    Calculate many padding arrays and/or how much padding
+  //    Join hits
+  //    Populate array
+  //    Adding truncateStart and truncateEnd to hits with padding, start is not 0 and end is not itemArr.length - 1
+  //    Stringifying & returning that string
+
+  // Check if to keep all query wordsCut off hit array
+  if (caseKeepAllQueryWords(hitArr, properties)) {
+    cases.keepAllQueryWords = true
+  } else {
+    // this needs to be moved later
+    cases.keepAllQueryWords = false
+  }
+  console.log('properties and cases: ')
+  console.log(properties)
+  console.log(cases)
+
+  // ############################
+  // # The highlighter "switch" #
+  // ############################
+
+  if (!cases.truncate) {
+    // just return the highlighted itemArray
+    console.log('### Case - Just highlight ###')
+  } else if (cases.truncate && cases.keepAllQueryWords) {
+    // needs truncating, but keep all query words
+    console.log('### Case - Truncat but keep all query word hits ###')
+    // Calculate Padding start and end for each hit
+    hitArr = setPaddingStartEnd(hitArr, itemArr.length, properties)
+    hitArr = joinOverlappingTruncate(hitArr)
+    hitTruncatedArr = truncateHitArr(hitArr, itemArr, hitTruncatedArr)
+    item = getHighlightedString(hitTruncatedArr, properties.truncateStart, properties.truncateEnd, properties.divider)
+    console.log(item)
+  } else if (cases.truncate && !cases.keepAllQueryWords) {
+    // needs truncadting and have to cut off query wors
+    console.log('### Case - Truncat and cut off some query word hits ###')
+    hitArr = cutOffHitArray(hitArr, properties)
+    hitArr = setPaddingStartEnd(hitArr, itemArr.length, properties)
+    hitArr = joinOverlappingTruncate(hitArr)
+    hitTruncatedArr = truncateHitArr(hitArr, itemArr, hitTruncatedArr)
+    item = getHighlightedString(hitTruncatedArr, properties.truncateStart, properties.truncateEnd, properties.divider)
+    console.log(item)
+  } else {
+    // Something I didn't think of, returning the highlighted itemArray
+    console.log('### Case - Not sure how you ended up here. Debug!  ###')
+  }
+
+  // Get highlighted item as as string
+  
+}
+
+// ###################################
+// # The Functions to make it happen #
+// ###################################
+
+// Function: figure out if itemArr needs to be truncated
+const caseTruncate = function (itemArrLength, properties, cases) {
+  if (itemArrLength > properties.itemMaxWords && properties.itemMaxWords !== 0) {
+    return true
+  }
+}
+
+// Function: Set highlightable query words and count how many
+const setHighlightables = function (itemArr, queryArr, cases) {
   itemArr = itemArr.map(function (itemWord, index) {
     const wordObj = {
       word: itemWord
     }
     if (queryArr.find(function (queryWord) { return itemWord === queryWord })) {
       wordObj.highlightable = true
-      hitCount += 1
+      cases.hitCount = cases.hitCount + 1
     }
-    // console.log(wordObj)
     return wordObj
   })
+  return itemArr
+}
 
-  // C: Joining neighbour highlightable words, removing redundant and setting index value
+// Function: Joining neighbour highlightable words, removing redundant and setting index value
+const joinNeighbourHighlightable = function (itemArr, properties) {
   for (let i = 0; i < itemArr.length; i++) {
     if (i > 0 && itemArr[i].highlightable && itemArr[i - 1].highlightable) {
       // Joining this and previous word
@@ -60,60 +161,52 @@ const highlight = function (queryArr, itemArr, properties) {
       itemArr[i].index = i
     }
   }
+  return itemArr
+}
 
+// Function: Add highlighting start and end to highlightable
+const addHighlighting = function (itemArr, properties) {
   itemArr = itemArr.map(function (itemWord, index) {
     if (itemWord.highlightable) {
       itemWord.word = properties.highlightStart + itemWord.word + properties.highlightEnd
     }
     return itemWord
   })
+  return itemArr
+}
 
-  // D: Defining index on highlightable words and pushing to itemArr
-  //    Adding highlightStart and highlightEnd to words / terms
-  hitArr = itemArr.filter(itemArr => itemArr.highlightable)
-  console.log(itemArr)
-  console.log(hitArr)
-
-  // E: Calculating padding array of arrays
-  //    Caluclataing how much padding
-  //    Calculate many padding arrays and/or how much padding
-  //    Join hits
-  //    Populate array
-  //    Adding truncateStart and truncateEnd to hits with padding, start is not 0 and end is not itemArr.length - 1
-  //    Stringifying & returning that string
-
-  console.log('Number of hits to be padded: ' + hitArr.length)
-  console.log('Number words hit: ' + hitCount)
-  console.log('Max words per item: ' + properties.itemMaxWords)
-
-  // Do a minimum calculation, based on hitPaddingMin, hitArr.length and properties.itemMaxWords
-  //  If less than or equal to itemMaxWords: do one more calculation, expanding hitPadding to something sensible
-  //  !!! Should have a test on only one query word
-  //  If more than itemMaxWords: calculate how many hits to use from hitArr
-  if (hitArr.length * (hitPadding * 2) <= properties.itemMaxWords) {
-    console.log('Keep the whole hitArr with padding')
-  } else if (hitArr.length * (hitPadding * 2) > properties.itemMaxWords) {
-    console.log('We need to cut off, but how much?\nHere is some calculation:')
-    // per hit
-    const totalWords = hitArr.length * ((hitPadding * 2) + 1)
-    const wordsPerHit = (hitPadding * 2) + 1
-    const wordsToMany = totalWords - properties.itemMaxWords
-    //  how many hits to cut off
-    const hitsToKeep = hitArr.length - (Math.ceil(wordsToMany / wordsPerHit))
-    console.log('How many hits to keep: ' + hitsToKeep)
-    hitArr = hitArr.slice(0, hitsToKeep)
-    console.log(hitArr)
-  }
-
-  // Padding etc
+// Just set index for padding start and end for each hit
+const setPaddingStartEnd = function (hitArr, itemArrLength, properties) {
   for (let i = 0; i < hitArr.length; i++) {
     // do some padding stuff...
-    hitArr[i].paddStart = Math.max(hitArr[i].index - hitPadding, 0)
-    hitArr[i].paddEnd = Math.min(hitArr[i].index + hitPadding + 1, itemArr.length)
+    hitArr[i].paddStart = Math.max(hitArr[i].index - properties.hitPaddingMin, 0)
+    hitArr[i].paddEnd = Math.min(hitArr[i].index + properties.hitPaddingMin + 1, itemArrLength)
   }
-  console.log(hitArr)
+  return hitArr
+}
 
-  // finding boundaries for paddingGroups
+// Check if not to keep all query word hits
+const caseKeepAllQueryWords = function (hitArr, properties) {
+  if (hitArr.length * (properties.hitPaddingMin * 2) > properties.itemMaxWords) {
+    return false
+  } else {
+    return true
+  }
+}
+
+// Cut off hit array
+const cutOffHitArray = function (hitArr, properties) {
+  // per hit
+  const totalWords = hitArr.length * ((properties.hitPaddingMin * 2) + 1)
+  const wordsPerHit = (properties.hitPaddingMin * 2) + 1
+  const wordsToMany = totalWords - properties.itemMaxWords
+  //  how many hits to cut off
+  const hitsToKeep = hitArr.length - (Math.ceil(wordsToMany / wordsPerHit))
+  hitArr = hitArr.slice(0, hitsToKeep)
+  return hitArr
+}
+
+const joinOverlappingTruncate = function (hitArr) {
   for (let i = 0; i < hitArr.length; i++) {
     if (i > 0 && hitArr[i].paddStart <= hitArr[i - 1].paddEnd) {
       // join this and next
@@ -122,31 +215,31 @@ const highlight = function (queryArr, itemArr, properties) {
       hitArr.splice(i - 1, 1)
       // fixing array count
       i = i - 1
-      console.log('processed: ' + JSON.stringify(hitArr[i]))
     }
   }
-  console.log(hitArr)
-
-  // slice itemArr with info from hitArr and push to hitPaddedArr
-  for (let i = 0; i < hitArr.length; i++) {
-    hitPaddedArr[i] = itemArr.slice(hitArr[i].paddStart, hitArr[i].paddEnd)
-  }
-  const hit = getHighlightedString(hitPaddedArr, properties.truncateStart, properties.truncateEnd, properties.divider)
-  console.log(hit)
+  return hitArr
 }
 
-const getHighlightedString = function (hitPaddedArr, truncateStart, truncateEnd, divider) {
-  // console.log(hitPaddedArr)
-  let hitPadded = []
-  for (let i = 0; i < hitPaddedArr.length; i++) {
-    let singleHitPadded = ''
-    for (let j = 0; j < hitPaddedArr[i].length; j++) {
-      console.log(hitPaddedArr[i][j])
-      singleHitPadded += hitPaddedArr[i][j].word + divider
-    }
-    hitPadded += truncateStart + singleHitPadded + truncateEnd
+// Generate Truncate groups
+const truncateHitArr = function (hitArr, itemArr, hitTruncatedArr) {
+  for (let i = 0; i < hitArr.length; i++) {
+    hitTruncatedArr[i] = itemArr.slice(hitArr[i].paddStart, hitArr[i].paddEnd)
   }
-  return hitPadded
+  return hitTruncatedArr
+}
+
+// Create the string with hit highligted, truncated and padded
+// Guessing this may need to have it's siblings for not truncated ???
+const getHighlightedString = function (hitTruncatedArr, truncateStart, truncateEnd, divider) {
+  let hitTruncated = []
+  for (let i = 0; i < hitTruncatedArr.length; i++) {
+    let singleHitPadded = ''
+    for (let j = 0; j < hitTruncatedArr[i].length; j++) {
+      singleHitPadded += hitTruncatedArr[i][j].word + divider
+    }
+    hitTruncated += truncateStart + singleHitPadded + truncateEnd
+  }
+  return hitTruncated
 }
 
 highlight(query, result, properties)
